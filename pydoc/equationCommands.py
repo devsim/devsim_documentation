a7f714991206afbdbaacc76077372c6c2951bbed
Juan E. Sanchez <juan@tcad.com>
2020-09-10 20:24:11 -0500

    fix typo

--- pydoc/equationCommands.py
+++ pydoc/equationCommands.py
@@ -41,6 +41,7 @@ The integration variables can be changed in 2D for cylindrical coordinate system
 In order to set the node volumes for integration of the ``edge_volume_model``, it is possible to do something like this:
 
 ..
+
   devsim.edge_model(device="device", region="region", name="EdgeNodeVolume", equation="0.5*SurfaceArea*EdgeLength")
   devsim.set_parameter(name="edge_node0_volume_model", value="EdgeNodeVolume")
   devsim.set_parameter(name="edge_node1_volume_model", value="EdgeNodeVolume")
