a1ef0c93ef9c43cd40ae7522dcf6a6b13a430be5
Juan E. Sanchez <juan@tcad.com>
2020-09-13 10:51:23 -0500

    fix typo

--- pydoc/devsim.py
+++ pydoc/devsim.py
@@ -276,6 +276,7 @@ def equation (**kwargs):
     In order to set the node volumes for integration of the ``edge_volume_model``, it is possible to do something like this:
 
     ..
+
       devsim.edge_model(device="device", region="region", name="EdgeNodeVolume", equation="0.5*SurfaceArea*EdgeLength")
       devsim.set_parameter(name="edge_node0_volume_model", value="EdgeNodeVolume")
       devsim.set_parameter(name="edge_node1_volume_model", value="EdgeNodeVolume")
