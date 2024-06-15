5d910e90fab3f685e20476f6eefa88c63b4849f5
Juan E. Sanchez <juan@tcad.com>
2024-06-14 22:57:53 -0500

    modify install script

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-set -e
-conda create --yes --name devsim_doc python=3
-source activate devsim_doc
-conda install --yes sphinx sphinx_rtd_theme numpydoc
-pip install sphinxcontrib-bibtex
-pip install sphinx_sitemap
+pip install sphinx sphinx_rtd_theme numpydoc sphinxcontrib-bibtex sphinx_sitemap
